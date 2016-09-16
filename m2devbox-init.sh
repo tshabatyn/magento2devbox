d02fa2be766a79520522aba07e207a5bc7369d49
Yuri Kovsher <ikovsher@magento.com>
2016-09-16 12:17:31 +0300

    MAGETWO-57922: Split install.sh command on magento-devbox-prepare and magento-devbox

--- m2devbox-init.sh
+++ m2devbox-init.sh
@@ -2,10 +2,11 @@
 
 get_free_port () {
     local port=$1
-    while [[ $(lsof -i tcp:$port | grep "(LISTEN)") ]]
-    do
+
+    while [[ $(lsof -i tcp:$port | grep "(LISTEN)") ]]; do
         port=$RANDOM
     done
+
     echo $port
 }
 
@@ -85,17 +86,66 @@ done
 
 echo 'Creating docker-compose config'
 
-if [[ $webroot_path ]]; then
+#Database
+db_host='db'
+db_password='root'
+db_user='root'
+db_name='magento2'
+db_port=3306
+db_home_port=$(get_free_port 1345)
+db_path='/var/lib/mysql'
+
+if [[ ! $db_home_path ]]; then
+    db_home_path='./shared/db'
+fi
+
+#RabbitMQ
+rabbitmq_host='rabbit'
+rabbitmq_port=5672
+rabbitmq_admin_port=15672
+rabbitmq_home_port=$(get_free_port $rabbitmq_port)
+rabbitmq_home_admin_port=$(get_free_port 8282)
+
+#Redis
+redis_host='redis'
+
+#Varnish
+varnish_container=magento2-devbox-varnish
+varnish_port=6081
+varnish_home_port=$(get_free_port 1748)
+varnish_config_path='/home/magento2/scripts/default.vcl'
+
+#Elastic Search
+elastic_host='elasticsearch'
+elastic_port=9200
+elastic_home_port=$(get_free_port 9200)
+
+#Web Server
+webserver_container=magento2-devbox-web
+webserver_host=web
+webserver_port=80
+webserver_ssh_port=22
+webserver_home_port=$(get_free_port 1749)
+webserver_home_ssh_port=$(get_free_port 2222)
+
+#Paths
+magento_path='/var/www/magento2'
+magento_cloud_path='./shared/.magento-cloud'
+magento_cloud_home_path='/root/.magento-cloud'
+composer_home_path='/home/magento2/.composer'
+ssh_home_path='/home/magento2/.ssh'
+
+if [[ $magento_home_path ]]; then
     magento_sources_reuse=1
 else
-    webroot_path='./shared/webroot'
+    magento_home_path='./shared/webroot'
 
     if [[ ! $magento_sources_reuse ]]; then
         request 'magento_sources_reuse' 'Do you have existing copy of Magento 2?' 1
     fi
 
     if [[ $magento_sources_reuse = 1 ]]; then
-        request 'webroot_path' 'Please provide full path to the Magento folder on local machine'
+        request 'magento_home_path' 'Please provide full path to the Magento folder on local machine'
     fi
 fi
 
@@ -106,17 +156,6 @@ fi
 if [[ ! $ssh_path ]]; then
     ssh_path='./shared/.ssh'
 fi
-
-if [[ ! $db_path ]]; then
-    db_path='./shared/db'
-fi
-
-db_host=db
-db_port=3306
-db_password=root
-db_user=root
-db_name=magento2
-db_host_port=$(get_free_port 1345)
 
 cat > docker-compose.yml <<- EOM
 ##
@@ -131,82 +170,49 @@ services:
     restart: always
     image: mysql:5.6
     ports:
-      - "$db_host_port:$db_port"
+      - $db_home_port:$db_port
     environment:
       - MYSQL_ROOT_PASSWORD=$db_password
       - MYSQL_DATABASE=$db_name
     volumes:
-      - $db_path:/var/lib/mysql
+      - $db_home_path:$db_path
       - ./shared/logs/mysql:/var/log/mysql
-EOM
-
-rabbit_host='rabbit'
-rabbit_port=5672
-rabbit_admin_host_port=$(get_free_port 8282)
-rabbit_host_port=$(get_free_port $rabbit_port)
-  cat << EOM >> docker-compose.yml
-  $rabbit_host:
+  $rabbitmq_host:
     container_name: magento2-devbox-rabbit
     image: rabbitmq:3-management
     ports:
-      - "$rabbit_admin_host_port:15672"
-      - "$rabbit_host_port:$rabbit_port"
-EOM
-
-redis_host='redis'
-
-cat << EOM >> docker-compose.yml
+      - $rabbitmq_home_admin_port:$rabbitmq_admin_port
+      - $rabbitmq_home_port:$rabbitmq_port
   $redis_host:
     container_name: magento2-devbox-redis
     image: redis:3.0.7
-EOM
-
-varnish_host_port=$(get_free_port 1748)
-varnish_host_container=magento2-devbox-varnish
-
-cat << EOM >> docker-compose.yml
   varnish:
     image: magento/magento2devbox_varnish:latest
-    container_name: $varnish_host_container
+    container_name: $varnish_container
     ports:
-      - "$varnish_host_port:6081"
-EOM
-
-elastic_host='elasticsearch'
-elastic_port=9200
-elastic_local_port=$(get_free_port 9200)
-cat << EOM >> docker-compose.yml
+      - $varnish_home_port:$varnish_port
   $elastic_host:
     image: elasticsearch:latest
     container_name: magento2-devbox-elasticsearch
     ports:
-      - "$elastic_local_port:$elastic_port"
-EOM
-
-magento_path='/var/www/magento2'
-webserver_host=web
-webserver_port=80
-webserver_container=magento2-devbox-web
-web_port=$(get_free_port 1749)
-
-cat << EOM >> docker-compose.yml
+      - $elastic_home_port:$elastic_port
   $webserver_host:
-    # image: magento/magento2devbox_web:latest
+#    image: magento/magento2devbox_web:latest
     build: web
     container_name: $webserver_container
     volumes:
-      - $webroot_path:$magento_path
-      - $composer_path:/home/magento2/.composer
-      - $ssh_path:/home/magento2/.ssh
+      - $magento_home_path:$magento_path
+      - $composer_path:$composer_home_path
+      - $ssh_path:$ssh_home_path
       - ./shared/logs/apache2:/var/log/apache2
       - ./shared/logs/php-fpm:/var/log/php-fpm
-      #    - ./shared/.magento-cloud:/root/.magento-cloud
+#      - $magento_cloud_path:$magento_cloud_home_path
     ports:
-      - "$web_port:$webserver_port"
-      - "2222:22"
+      - $webserver_home_port:$webserver_port
+      - $webserver_home_ssh_port:$webserver_ssh_port
 EOM
 
-echo 'Creating shared folders'
+echo 'Creating shared and temporary folders'
 mkdir -p shared/.composer
 mkdir -p shared/.ssh
 mkdir -p shared/webroot
@@ -214,6 +220,7 @@ mkdir -p shared/db
 mkdir -p shared/logs/apache2
 mkdir -p shared/logs/php-fpm
 mkdir -p shared/logs/mysql
+mkdir -p tmp
 
 echo 'Build docker images'
 docker-compose up --build -d
@@ -225,22 +232,22 @@ docker exec -it --privileged -u magento2 magento2-devbox-web \
     php -f /home/magento2/scripts/m2init magento:install \
         --magento-sources-reuse=$magento_sources_reuse \
         --magento-path=$magento_path \
-        --rabbitmq-host=$rabbit_host \
-        --rabbitmq-port=$rabbit_port \
-        --redis-host=$redis_host \
+        --webserver-host=$webserver_host \
+        --webserver-port=$webserver_port \
         --db-host=$db_host \
         --db-port=$db_port \
         --db-user=$db_user \
         --db-name=$db_name \
         --db-password=$db_password \
-        --webserver-host=$webserver_host \
-        --webserver-port=$webserver_port \
-        --varnish-config-path=/home/magento2/scripts/default.vcl \
+        --rabbitmq-host=$rabbitmq_host \
+        --rabbitmq-port=$rabbitmq_port \
+        --redis-host=$redis_host \
+        --varnish-config-path=$varnish_config_path \
         --elastic-host=$elastic_host \
         --elastic-port=$elastic_port
 
-docker cp "$webserver_container:/home/magento2/scripts/default.vcl" ./default.vcl.bak
-docker cp ./default.vcl.bak $varnish_host_container:/etc/varnish/default.vcl
-rm ./default.vcl.bak
+docker cp $webserver_container:/home/magento2/scripts/default.vcl tmp/default.vcl.bak
+docker cp tmp/default.vcl.bak $varnish_container:/etc/varnish/default.vcl
 
 docker-compose restart varnish
+rm -rf tmp
