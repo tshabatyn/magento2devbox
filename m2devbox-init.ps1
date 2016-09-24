85905da9ca2a5675ed81a7b799bc6cc3f6386b98
Dmytro Poperechnyy <dpoperechnyy@magento.com>
2016-09-24 12:08:51 +0300

    MAGETWO-58856: Transfer m2devbox-init bash script to powershell script
    - Updated magento2 devbox init powershell script;

--- m2devbox-init.ps1
+++ m2devbox-init.ps1
@@ -1,8 +1,20 @@
-if (Test-Path tmp) {
-    Remove-Item tmp -Recurse -Force
+if (Test-Path data/options) {
+    Remove-Item data/options -Recurse -Force
+}
+if (Test-Path data/ports) {
+    Remove-Item data/ports -Recurse -Force
 }
 
-function get_data ($file_name, $folder_path='tmp', $file_path="$folder_path/$file_name", $contents) {
+function generate_container_name ($service, $number, $name="magento2devbox_${service}_${number}") {
+    while (docker ps -a -q --filter="name=$name") {
+        $number++
+        $name = "magento2devbox_${service}_${number}"
+    }
+
+    return $name
+}
+
+function get_data ($file_name, $folder_path='data', $file_path="$folder_path/$file_name", $contents) {
     if (Test-Path $file_path) {
         $contents=$(type $file_path)
     }
@@ -12,7 +24,7 @@ function get_data ($file_name, $folder_path='tmp', $file_path="$folder_path/$fil
 
 function store_data
     ($file_name, $value, $delimiter, $key, $key_value_delimiter, $prefix,
-    $suffix, $folder_path = 'tmp', $file_path = "$folder_path/$file_name", $contents = (get_data $file_name))
+    $suffix, $folder_path = 'data', $file_path = "$folder_path/$file_name", $contents = (get_data $file_name))
     {
     if ($contents) {
         $contents = "$contents$delimiter"
@@ -26,7 +38,10 @@ function store_data
 
     $contents="$contents$value$suffix"
 
-    New-Item -ItemType directory -Path $folder_path
+    if (!(Test-Path -Path $folder_path)) {
+        New-Item -ItemType directory -Path $folder_path
+    }
+
     $contents | Out-File $file_path
 
     return $value | Out-Null
@@ -96,6 +111,7 @@ function request ($varName, $question, $isBoolean, $defaultValue, $value, $outpu
 Write-Host "Creating docker-compose config"
 
 #Database
+$db_container = generate_container_name 'db'
 $db_user = store_option 'db-user' 'root'
 $db_host = store_option 'db-host' 'db'
 $db_password = store_option 'db-password' 'root'
@@ -111,6 +127,7 @@ if (!$db_home_path) {
 }
 
 #RabbitMQ
+$rabbitmq_container = generate_container_name 'rabbit'
 $rabbitmq_host = store_option 'rabbitmq-host' 'rabbit'
 $rabbitmq_port = store_option 'rabbitmq-port' 5672
 $rabbitmq_admin_port = 15672
@@ -118,37 +135,48 @@ $rabbitmq_home_port = get_free_port 5672
 $rabbitmq_home_admin_port = get_free_port 8282
 
 #Redis
+$redis_container = generate_container_name 'redis'
 $redis_host = store_option 'redis-host' 'redis'
 
 #Varnish
+$varnish_container = generate_container_name 'varnish'
+$varnish_host = 'varnish'
 $varnish_port = 6081
-$varnish_home_port = get_free_port 1748
+$varnish_home_port = get_free_port 1749
+store_option 'varnish-home-port' $varnish_home_port | Out-Null
 $varnish_config_dir = '/home/magento2/configs/varnish'
 $varnish_config_path = store_option 'varnish-config-path' "$varnish_config_dir/default.vcl"
 $varnish_shared_dir = "./shared/configs/varnish"
 $varnish_container_config_path = '/etc/varnish/default'
 
 #Elastic Search
+$elastic_container = generate_container_name 'elastic'
 $elastic_host = store_option 'elastic-host' 'elasticsearch'
 $elastic_port = store_option 'elastic-port' 9200
 $elastic_home_port = get_free_port 9200
 
 #Web Server
+$webserver_container = generate_container_name 'web'
 $webserver_host = store_option 'webserver-host' 'web'
 $webserver_port = store_option 'webserver-port' 80
 $webserver_ssh_port = 22
-$webserver_home_port = get_free_port 1749
+$webserver_home_port = get_free_port 1748
+store_option 'webserver-home-port' $webserver_home_port | Out-Null
 $webserver_home_ssh_port = get_free_port 2222
 $webserver_apache_logs_path = '/var/log/apache2'
 $webserver_phpfpm_logs_path = '/var/log/php-fpm'
 $webserver_home_apache_logs_path = './shared/logs/apache2'
 $webserver_home_phpfpm_logs_path = './shared/logs/php-fpm'
 
-#Paths
-$magento_path = store_option 'magento-path' '/var/www/magento2'
+#Magento
+$magento_host = store_option 'magento-host' 'localhost'
+$magento_path = store_option 'magento-path' '/home/magento2/magento2'
 $magento_cloud_path = '/root/.magento-cloud'
+$magento_cloud_home_path = './shared/.magento-cloud'
 $composer_path = '/home/magento2/.composer'
+$composer_home_path = './shared/.composer'
 $ssh_path = '/home/magento2/.ssh'
+$ssh_home_path = './shared/.ssh'
 
 if ($Args.Length -gt 0) {
     foreach ($argument in $Args) {
@@ -177,28 +205,23 @@ if ($Args.Length -gt 0) {
 }
 
 if ($magento_home_path) {
-    $magento_sources_reuse = $true
+    $magento_sources_reuse = 1
 } else {
     $magento_home_path='./shared/webroot'
 
     if (!$magento_sources_reuse) {
-        request 'magento_sources_reuse' 'Do you have existing copy of Magento 2?' $true
+        request 'magento_sources_reuse' 'Do you have existing copy of Magento 2?' 1
     }
-    if ($magento_sources_reuse -eq $true) {
-        request 'magento_home_path' 'Please provide full path to the Magento folder on local machine'
+    if ($magento_sources_reuse -eq 1) {
+        $magento_default_home_path = get_data 'magento_home_path'
+
+        request 'magento_home_path' 'Please provide full path to the Magento folder on local machine' 0 $magento_default_home_path
+
+        if (!magento_home_path) {
+            echo "Error: Magento folder was not specified!"
+        }
     }
-}
-
-if (!$magento_cloud_home_path) {
-    $magento_cloud_home_path = './shared/.magento-cloud'
-}
-
-if (!$composer_home_path) {
-    $composer_home_path = './shared/.composer'
-}
-
-if (!$ssh_home_path) {
-    $ssh_home_path = './shared/.ssh'
+    store_data 'magento_home_path' $magento_home_path | Out-Null
 }
 
 store_option 'magento-sources-reuse' $magento_sources_reuse | Out-Null
@@ -211,36 +234,9 @@ $yml = @"
 ##
 version: '2'
 services:
-  %%%DB_HOST%%%:
-      restart: always
-      image: mysql:5.6
-      ports:
-          - "%%%DB_HOME_PORT%%%:%%%DB_PORT%%%"
-      environment:
-          - MYSQL_ROOT_PASSWORD=%%%DB_PASSWORD%%%
-          - MYSQL_DATABASE=%%%DB_NAME%%%
-      volumes:
-          - "%%%DB_HOME_PATH%%%:%%%DB_PATH%%%"
-          - "%%%DB_HOME_LOGS_PATH%%%:%%%DB_LOGS_PATH%%%"
-  %%%RABBITMQ_HOST%%%:
-      image: rabbitmq:3-management
-      ports:
-          - "%%%RABBITMQ_HOME_ADMIN_PORT%%%:%%%RABBITMQ_ADMIN_PORT%%%"
-          - "%%%RABBITMQ_HOME_PORT%%%:%%%RABBITMQ_PORT%%%"
-  %%%REDIS_HOST%%%:
-      image: redis:3.0.7
-  varnish:
-#      image: magento/magento2devbox_varnish:latest
-      build: varnish
-      volumes:
-          - "%%%VARNISH_SHARED_DIR%%%:%%%VARNISH_CONTAINER_CONFIG_PATH%%%"
-      ports:
-          - "%%%VARNISH_HOME_PORT%%%:%%%VARNISH_PORT%%%"
-  %%%ELASTIC_HOST%%%:
-      image: elasticsearch:latest
-      ports:
-          - "%%%ELASTIC_HOME_PORT%%%:%%%ELASTIC_PORT%%%"
   %%%WEBSERVER_HOST%%%:
+      container_name: %%%WEBSERVER_CONTAINER%%%
+      restart: always
 #     image: magento/magento2devbox_web:latest
       build: web
       volumes:
@@ -251,34 +247,56 @@ services:
           - "%%%WEBSERVER_HOME_PHPFPM_LOGS_PATH%%%:%%%WEBSERVER_PHPFPM_LOGS_PATH%%%"
           - "%%%VARNISH_SHARED_DIR%%%:%%%VARNISH_CONFIG_DIR%%%"
           - "%%%MAGENTO_CLOUD_HOME_PATH%%%:%%%MAGENTO_CLOUD_PATH%%%"
+      environment:
+          - USE_SHARED_WEBROOT=0
+          - SHARED_CODE_PATH="%%%MAGENTO_PATH%%%"
       ports:
           - "%%%WEBSERVER_HOME_PORT%%%:%%%WEBSERVER_PORT%%%"
           - "%%%WEBSERVER_HOME_SSH_PORT%%%:%%%WEBSERVER_SSH_PORT%%%"
+  %%%DB_HOST%%%:
+      container_name: %%%DB_CONTAINER%%%
+      restart: always
+      image: mysql:5.6
+      ports:
+          - "%%%DB_HOME_PORT%%%:%%%DB_PORT%%%"
+      environment:
+          - MYSQL_ROOT_PASSWORD=%%%DB_PASSWORD%%%
+          - MYSQL_DATABASE=%%%DB_NAME%%%
+      volumes:
+          - "%%%DB_HOME_PATH%%%:%%%DB_PATH%%%"
+          - "%%%DB_HOME_LOGS_PATH%%%:%%%DB_LOGS_PATH%%%"
+  %%%VARNISH_HOST%%%:
+        container_name: %%%VARNISH_CONTAINER%%%
+        restart: always
+        depends_on:
+           - %%%WEBSERVER_HOST%%%
+  #     image: magento/magento2devbox_varnish:latest
+        build: varnish
+        volumes:
+            - "%%%VARNISH_SHARED_DIR%%%:%%%VARNISH_CONTAINER_CONFIG_PATH%%%"
+        ports:
+            - "%%%VARNISH_HOME_PORT%%%:%%%VARNISH_PORT%%%"
+  %%%REDIS_HOST%%%:
+        container_name: %%%REDIS_CONTAINER%%%
+        restart: always
+        image: redis:3.0.7
+  %%%RABBITMQ_HOST%%%:
+      container_name: %%%RABBITMQ_CONTAINER%%%
+      restart: always
+      image: rabbitmq:3-management
+      ports:
+          - "%%%RABBITMQ_HOME_ADMIN_PORT%%%:%%%RABBITMQ_ADMIN_PORT%%%"
+          - "%%%RABBITMQ_HOME_PORT%%%:%%%RABBITMQ_PORT%%%"
+  %%%ELASTIC_HOST%%%:
+      container_name: %%%ELASTIC_CONTAINER%%%
+      restart: always
+      image: elasticsearch:latest
+      ports:
+          - "%%%ELASTIC_HOME_PORT%%%:%%%ELASTIC_PORT%%%"
 "@
 
-$yml = $yml -Replace "%%%DB_HOST%%%", $db_host
-$yml = $yml -Replace "%%%DB_HOME_PORT%%%", $db_home_port
-$yml = $yml -Replace "%%%DB_PORT%%%", $db_port
-$yml = $yml -Replace "%%%DB_PASSWORD%%%", $db_password
-$yml = $yml -Replace "%%%DB_NAME%%%", $db_name
-$yml = $yml -Replace "%%%DB_HOME_PATH%%%", $db_home_path
-$yml = $yml -Replace "%%%DB_PATH%%%", $db_path
-$yml = $yml -Replace "%%%DB_HOME_LOGS_PATH%%%", $db_home_logs_path
-$yml = $yml -Replace "%%%DB_LOGS_PATH%%%", $db_logs_path
-$yml = $yml -Replace "%%%RABBITMQ_HOST%%%", $rabbitmq_host
-$yml = $yml -Replace "%%%RABBITMQ_HOME_ADMIN_PORT%%%", $rabbitmq_home_admin_port
-$yml = $yml -Replace "%%%RABBITMQ_ADMIN_PORT%%%", $rabbitmq_admin_port
-$yml = $yml -Replace "%%%RABBITMQ_HOME_PORT%%%", $rabbitmq_home_port
-$yml = $yml -Replace "%%%RABBITMQ_PORT%%%", $rabbitmq_port
-$yml = $yml -Replace "%%%REDIS_HOST%%%", $redis_host
-$yml = $yml -Replace "%%%VARNISH_SHARED_DIR%%%", $varnish_shared_dir
-$yml = $yml -Replace "%%%VARNISH_CONTAINER_CONFIG_PATH%%%", $varnish_container_config_path
-$yml = $yml -Replace "%%%VARNISH_HOME_PORT%%%", $varnish_home_port
-$yml = $yml -Replace "%%%VARNISH_PORT%%%", $varnish_port
-$yml = $yml -Replace "%%%ELASTIC_HOST%%%", $elastic_host
-$yml = $yml -Replace "%%%ELASTIC_HOME_PORT%%%", $elastic_home_port
-$yml = $yml -Replace "%%%ELASTIC_PORT%%%", $elastic_port
 $yml = $yml -Replace "%%%WEBSERVER_HOST%%%", $webserver_host
+$yml = $yml -Replace "%%%WEBSERVER_CONTAINER%%%", $webserver_container
 $yml = $yml -Replace "%%%MAGENTO_HOME_PATH%%%", $magento_home_path
 $yml = $yml -Replace "%%%MAGENTO_PATH%%%", $magento_path
 $yml = $yml -Replace "%%%COMPOSER_HOME_PATH%%%", $composer_home_path
@@ -296,6 +314,34 @@ $yml = $yml -Replace "%%%WEBSERVER_HOME_PORT%%%", $webserver_home_port
 $yml = $yml -Replace "%%%WEBSERVER_PORT%%%", $webserver_port
 $yml = $yml -Replace "%%%WEBSERVER_HOME_SSH_PORT%%%", $webserver_home_ssh_port
 $yml = $yml -Replace "%%%WEBSERVER_SSH_PORT%%%", $webserver_ssh_port
+$yml = $yml -Replace "%%%DB_HOST%%%", $db_host
+$yml = $yml -Replace "%%%DB_CONTAINER%%%", $db_container
+$yml = $yml -Replace "%%%DB_HOME_PORT%%%", $db_home_port
+$yml = $yml -Replace "%%%DB_PORT%%%", $db_port
+$yml = $yml -Replace "%%%DB_PASSWORD%%%", $db_password
+$yml = $yml -Replace "%%%DB_NAME%%%", $db_name
+$yml = $yml -Replace "%%%DB_HOME_PATH%%%", $db_home_path
+$yml = $yml -Replace "%%%DB_PATH%%%", $db_path
+$yml = $yml -Replace "%%%DB_HOME_LOGS_PATH%%%", $db_home_logs_path
+$yml = $yml -Replace "%%%DB_LOGS_PATH%%%", $db_logs_path
+$yml = $yml -Replace "%%%VARNISH_HOST%%%", $varnish_host
+$yml = $yml -Replace "%%%VARNISH_CONTAINER%%%", $varnish_container
+$yml = $yml -Replace "%%%VARNISH_SHARED_DIR%%%", $varnish_shared_dir
+$yml = $yml -Replace "%%%VARNISH_CONTAINER_CONFIG_PATH%%%", $varnish_container_config_path
+$yml = $yml -Replace "%%%VARNISH_HOME_PORT%%%", $varnish_home_port
+$yml = $yml -Replace "%%%VARNISH_PORT%%%", $varnish_port
+$yml = $yml -Replace "%%%REDIS_HOST%%%", $redis_host
+$yml = $yml -Replace "%%%REDIS_CONTAINER%%%", $redis_container
+$yml = $yml -Replace "%%%RABBITMQ_HOST%%%", $rabbitmq_host
+$yml = $yml -Replace "%%%RABBITMQ_CONTAINER%%%", $rabbitmq_container
+$yml = $yml -Replace "%%%RABBITMQ_HOME_ADMIN_PORT%%%", $rabbitmq_home_admin_port
+$yml = $yml -Replace "%%%RABBITMQ_ADMIN_PORT%%%", $rabbitmq_admin_port
+$yml = $yml -Replace "%%%RABBITMQ_HOME_PORT%%%", $rabbitmq_home_port
+$yml = $yml -Replace "%%%RABBITMQ_PORT%%%", $rabbitmq_port
+$yml = $yml -Replace "%%%ELASTIC_HOST%%%", $elastic_host
+$yml = $yml -Replace "%%%ELASTIC_CONTAINER%%%", $elastic_container
+$yml = $yml -Replace "%%%ELASTIC_HOME_PORT%%%", $elastic_home_port
+$yml = $yml -Replace "%%%ELASTIC_PORT%%%", $elastic_port
 Set-Content docker-compose.yml $yml
 
 Write-Host "Creating shared folders"
@@ -328,8 +374,36 @@ Write-Host "Build docker images"
 
 docker-compose up --build -d
 
-$webserver_container = `docker-compose ps -q $webserver_host`
-docker exec -it --privileged $webserver_container /bin/sh -c "chown -R magento2:magento2 /home/magento2 && chown -R magento2:magento2 $magento_path"
+$ps1 = @"
+if (%%%ARGS%%%.Length -gt 0) {
+    switch (%%%ARGS%%%[0])
+    {
+        exec { %%%COMMAND%%% = %%%ARGS%%%[0] }
+        show-name { %%%COMMAND%%% = %%%ARGS%%%[0] }
+        default { echo 'Wrong command: only "exec" and "show-name" are supported' }
+    }
+
+    [System.Collections.ArrayList]%%%ARRAYLIST%%% = %%%ARGS%%%
+    %%%ARRAYLIST%%%.RemoveAt(0)
+
+    %%%OPTIONS%%% = "%%%ARRAYLIST%%%"
+
+    if (%%%COMMAND%%% -eq 'exec') {
+        docker exec -it --privileged -u magento2 %%%WEBSERVER_CONTAINER%%% %%%OPTIONS%%%
+    }
+
+    if (%%%COMMAND%%% -eq 'show-name') {
+        echo %%%WEBSERVER_CONTAINER%%%
+    }
+}
+"@
+
+$ps1 = $ps1 -Replace "%%%ARRAYLIST%%%", {$ArrayList}
+$ps1 = $ps1 -Replace "%%%ARGS%%%", {$Args}
+$ps1 = $ps1 -Replace "%%%COMMAND%%%", {$command}
+$ps1 = $ps1 -Replace "%%%OPTIONS%%%", {$options}
+$ps1 = $ps1 -Replace "%%%WEBSERVER_CONTAINER%%%", $webserver_container
+Set-Content m2devbox.ps1 $ps1
 
 $options = get_data 'options'
 
@@ -337,9 +411,7 @@ if ($interactive -ne 1) {
     $options = "$options --no-interaction"
 }
 
-docker exec -it --privileged -u magento2 $webserver_container php -f /home/magento2/scripts/m2init magento:install $options
-
-Remove-Item tmp -Recurse -Force
+./m2devbox.ps1 exec php -f /home/magento2/scripts/m2init magento:install $options
 
 clear-variable -name magento_home_path
 clear-variable -name magento_sources_reuse
